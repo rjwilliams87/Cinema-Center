30fdbc5a52b9ff697a7101e6264917009857e995
Ryan Williams <ryanjacobwilliams@gmail.com>
2019-02-20 13:44:09 -0600

    prettier

--- README.md
+++ README.md
@@ -1,25 +1,29 @@
-Cinema Center
-=============
-A single web page to watch the latest movie trailers, get information about new releases and find movie recommendations. 
+# Cinema Center
 
-Inspiration
------------
+A single web page to watch the latest movie trailers, get information about new releases and find movie recommendations.
+
+## Inspiration
+
 I've spent countless amounts of time searching various resources to find information about the latest movies. "When is the release
 date?" "How long is it?" "What's the viewer rating?" Cinema Center makes it easy for users to get all the information they need about
 the latest movies.
 
-Screenshots
------------
+## Screenshots
+
 ### Desktop
+
 ![fullpage screenshot](cincenter_photos/fullscreen.png "Desktop layout")
 
 #### Playlist search area
+
 ![playlist selection screen desktop](cincenter_photos/playlistdesktopscreencapture.png "playlist selection")
 
 #### Explore new releases or search for your favorite movie
+
 ![search area screen desktop](cincenter_photos/search_nowplayingdesktopscreenshot.png "Search and explore movies")
 
 #### Click posters to get more information about the selected movie
+
 ![lightbox pop up with informational text](cincenter_photos/infodisplayscreenshot.png "Lightbox movie information")
 
 ### Mobile
@@ -30,25 +34,25 @@ Screenshots
 
 ![potrait search area with movie posters](cincenter_photos/mobilesearchscreencapture.png "Potrait search movies area")
 
-Built with
-----------
-* HTML
-* CSS
-* JavaScript
-* jQuery
+## Built with
 
-Features
----------
-* Watch movie trailers by playlist
-* Explore new releases by category
-* Search for movies
-* Get information about selected movies
-* Explore similar titles based on search results
+- HTML
+- CSS
+- JavaScript
+- jQuery
 
-Live Demo
----------
+## Features
+
+- Watch movie trailers by playlist
+- Explore new releases by category
+- Search for movies
+- Get information about selected movies
+- Explore similar titles based on search results
+
+## Live Demo
+
 Demo: https://rjwilliams87.github.io/Cinema-Center/
 
-Author
--------
+## Author
+
 Ryan J. Williams
--- app.js
+++ app.js
@@ -1,63 +1,66 @@
-'use strict';
+"use strict";
 
 const apiStore = {
-    YouTube: {
-        URL: `https://www.googleapis.com/youtube/v3/playlistItems`,
-        settings: {
-            maxResults: 10,
-            playlistId: `PLScC8g4bqD47swdFI0NWMS7FPfjE6Nswy`,
-            part: 'snippet',
-            key: `AIzaSyBG6fsTfidnse58wUOpgcJ9d6PL9QiSMaM`, 
-            type: 'playlist',
-            order: 'date',
-          }
-    },
-    TMDB: {
-        URL: `https://api.themoviedb.org/3/movie`,
-        Search_URL: `https://api.themoviedb.org/3/search/movie`,
-        settings: 
-            {
-                api_key: `3d7040cc1b37d8215e343c54c3098fa6`,
-                language: 'en-US',
-                page: 1,
-              }
-    },
+  YouTube: {
+    URL: `https://www.googleapis.com/youtube/v3/playlistItems`,
+    settings: {
+      maxResults: 10,
+      playlistId: `PLScC8g4bqD47swdFI0NWMS7FPfjE6Nswy`,
+      part: "snippet",
+      key: `AIzaSyBG6fsTfidnse58wUOpgcJ9d6PL9QiSMaM`,
+      type: "playlist",
+      order: "date"
+    }
+  },
+  TMDB: {
+    URL: `https://api.themoviedb.org/3/movie`,
+    Search_URL: `https://api.themoviedb.org/3/search/movie`,
+    settings: {
+      api_key: `3d7040cc1b37d8215e343c54c3098fa6`,
+      language: "en-US",
+      page: 1
+    }
+  }
 };
 
-function getYouTubeApiData(callback){
-    $.getJSON(apiStore.YouTube.URL, apiStore.YouTube.settings, callback);
-  }
-
-function renderVideoResults(result){
-    return `
-    <div class="movie-trailer-thumbnail">
-      <iframe class="trailer" height="200" width="400" src="https://www.youtube.com/embed/${result.snippet.resourceId.videoId}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen
-      title="${result.snippet.title}" alt="movie trailer" aria-label=${result.snippet.title}></iframe>
-    </div>
-    `;
-  }
-
-function displayYoutubeData(data){
-    const videoResults = data.items.map((item) => renderVideoResults(item));
-    $('.trailer-content').html(videoResults);
-  }
-
-function handleYouTubePlaylistButton(){
-  $('.js-playlist-btn').click(function(e){
-    e.preventDefault();
-    const playlistId = $(this).attr('id')
-    apiStore.YouTube.settings.playlistId = `${playlistId}`;
-    getYouTubeApiData(displayYoutubeData);
-    searchResultsScrollAnimation($('.trailer-content'))
-  })
+function getYouTubeApiData(callback) {
+  $.getJSON(apiStore.YouTube.URL, apiStore.YouTube.settings, callback);
 }
 
-function fetch(url, callback){
-    $.getJSON(url, apiStore.TMDB.settings, callback)
-}  
+function renderVideoResults(result) {
+  return `
+    <div class="movie-trailer-thumbnail">
+      <iframe class="trailer" height="200" width="400" src="https://www.youtube.com/embed/${
+        result.snippet.resourceId.videoId
+      }" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen
+      title="${result.snippet.title}" alt="movie trailer" aria-label=${
+    result.snippet.title
+  }></iframe>
+    </div>
+    `;
+}
 
-function renderTMDBSearchResults(result){
-  const {title, id, poster_path} = result;
+function displayYoutubeData(data) {
+  const videoResults = data.items.map(item => renderVideoResults(item));
+  $(".trailer-content").html(videoResults);
+}
+
+function handleYouTubePlaylistButton() {
+  $(".js-playlist-btn").click(function(e) {
+    e.preventDefault();
+    const playlistId = $(this).attr("id");
+    apiStore.YouTube.settings.playlistId = `${playlistId}`;
+    getYouTubeApiData(displayYoutubeData);
+    searchResultsScrollAnimation($(".trailer-content"));
+  });
+}
+
+function fetch(url, callback) {
+  $.getJSON(url, apiStore.TMDB.settings, callback);
+}
+
+function renderTMDBSearchResults(result) {
+  const { title, id, poster_path } = result;
   return `
     <div class="search-results">
       <h2 class="poster-header">${title}</h2>
@@ -66,9 +69,9 @@ function renderTMDBSearchResults(result){
     </div>`;
 }
 
-function renderTMDBResults (result){
-  const {title, id, release_date, poster_path} = result;
-    return  `
+function renderTMDBResults(result) {
+  const { title, id, release_date, poster_path } = result;
+  return `
     <div class="in-theaters-info">
         <h2 class="poster-header">${title}</h2>
         <a href="#" id="${id}" title="click for more info"><img class="poster-image" src="https://image.tmdb.org/t/p/w300/${poster_path}" alt="poster ${title}"></a>
@@ -76,8 +79,8 @@ function renderTMDBResults (result){
     `;
 }
 
-function renderTMDBMovieDetails(result){
-  const {title, vote_average, runtime, release_date, overview} = result
+function renderTMDBMovieDetails(result) {
+  const { title, vote_average, runtime, release_date, overview } = result;
   return `
   <div class="movie-details">
     <h3 class="movie-details-header" tabindex="0">${title}</h3>
@@ -89,100 +92,123 @@ function renderTMDBMovieDetails(result){
   </div>`;
 }
 
-function displayTMDBSearchData(data){
-  const searchResults = data.results.map((item, index)=> renderTMDBSearchResults(item));
-  $('.recommendations-page').html(searchResults);
+function displayTMDBSearchData(data) {
+  const searchResults = data.results.map((item, index) =>
+    renderTMDBSearchResults(item)
+  );
+  $(".recommendations-page").html(searchResults);
 }
 
-function displayPlayingData(data){
-    const newReleaseResults = data.results.map((item)=> renderTMDBResults(item)).sort();
-    $('.now-playing-content').html(newReleaseResults);
+function displayPlayingData(data) {
+  const newReleaseResults = data.results
+    .map(item => renderTMDBResults(item))
+    .sort();
+  $(".now-playing-content").html(newReleaseResults);
 }
 
-function displayMovieDetailsData(data){
+function displayMovieDetailsData(data) {
   const results = renderTMDBMovieDetails(data);
-  $('.lightbox').html(results);
+  $(".lightbox").html(results);
 }
 
-function handleNowPlayingSearchButton(){
-  $('.now-playing-form').submit(function(e){
+function handleNowPlayingSearchButton() {
+  $(".now-playing-form").submit(function(e) {
     e.preventDefault();
-    const queryTarget = $(this).find('.userInput');
+    const queryTarget = $(this).find(".userInput");
     const query = queryTarget.val();
     fetch(`${apiStore.TMDB.URL}/${query}`, displayPlayingData);
-    searchResultsScrollAnimation($('.now-playing-content'));
+    searchResultsScrollAnimation($(".now-playing-content"));
   });
 }
 
-function handleSearchMovieButton(){
-  $('.js-search-form').submit(function(e){
+function handleSearchMovieButton() {
+  $(".js-search-form").submit(function(e) {
     e.preventDefault();
-    const queryTarget = $(this).find('.js-searchBar');
+    const queryTarget = $(this).find(".js-searchBar");
     const query = queryTarget.val();
-    queryTarget.val('');
+    queryTarget.val("");
     apiStore.TMDB.settings.query = query;
     fetch(apiStore.TMDB.Search_URL, displayTMDBSearchData);
-    searchResultsScrollAnimation($('.recommendations-page'));
-  });
-}
-  
-function handlerecommendationButton(){
-  $('.recommendations-page').on('click', '.js-search-recommended-button', function(e){
-    e.preventDefault();
-    const movieId = $(this).attr('id').match(/\d+/g).join('');
-    fetch(`${apiStore.TMDB.URL}/${movieId}/recommendations`, displayTMDBSearchData);
-    searchResultsScrollAnimation($('.recommendations-page'));
+    searchResultsScrollAnimation($(".recommendations-page"));
   });
 }
 
-function handleMovieInformationDisplay(){
-  $('.clickable-content').on('click', 'a', function(event){
+function handlerecommendationButton() {
+  $(".recommendations-page").on(
+    "click",
+    ".js-search-recommended-button",
+    function(e) {
+      e.preventDefault();
+      const movieId = $(this)
+        .attr("id")
+        .match(/\d+/g)
+        .join("");
+      fetch(
+        `${apiStore.TMDB.URL}/${movieId}/recommendations`,
+        displayTMDBSearchData
+      );
+      searchResultsScrollAnimation($(".recommendations-page"));
+    }
+  );
+}
+
+function handleMovieInformationDisplay() {
+  $(".clickable-content").on("click", "a", function(event) {
     event.preventDefault();
-    const movieId = $(this).attr('id');
-    $('.media-wrapper').append(`<div id="wrapper"></div>`);
-    $('#wrapper').fadeIn(function(){
-      $('.search-area-wrapper').append(`<div class="lightbox" aria-live="assertive"></div>`);
-      $('.lightbox').fadeIn();
-      fetch(`https://api.themoviedb.org/3/movie/${movieId}`, displayMovieDetailsData);
+    const movieId = $(this).attr("id");
+    $(".media-wrapper").append(`<div id="wrapper"></div>`);
+    $("#wrapper").fadeIn(function() {
+      $(".search-area-wrapper").append(
+        `<div class="lightbox" aria-live="assertive"></div>`
+      );
+      $(".lightbox").fadeIn();
+      fetch(
+        `https://api.themoviedb.org/3/movie/${movieId}`,
+        displayMovieDetailsData
+      );
     });
   });
 }
 
-function handleExitLightboxButton(){
-  $('body').on('click', '.exit-btn', function(e){
+function handleExitLightboxButton() {
+  $("body").on("click", ".exit-btn", function(e) {
     e.preventDefault();
-    $('.lightbox').fadeOut(function(){
-      $('#wrapper').remove();
+    $(".lightbox").fadeOut(function() {
+      $("#wrapper").remove();
     });
   });
 }
 
-function handleLightboxEscButton(){
-  $('body').keyup(function(e){
+function handleLightboxEscButton() {
+  $("body").keyup(function(e) {
     if (e.which == 27) {
-      $('.lightbox').fadeOut(function(){
-        $('#wrapper').remove()});
+      $(".lightbox").fadeOut(function() {
+        $("#wrapper").remove();
+      });
     }
   });
 }
 
-function handleHomeScrollButton(){
-  $('.js-scroll-to-content').click(function(e){
-      e.preventDefault();
-      $('html,body').animate({
-          scrollTop: $('.movie-content').offset().top
-      }, 800);
+function handleHomeScrollButton() {
+  $(".js-scroll-to-content").click(function(e) {
+    e.preventDefault();
+    $("html,body").animate(
+      {
+        scrollTop: $(".movie-content").offset().top
+      },
+      800
+    );
   });
 }
 
-function searchResultsScrollAnimation(element){
-  element.animate({scrollTop: 0}, 800);
+function searchResultsScrollAnimation(element) {
+  element.animate({ scrollTop: 0 }, 800);
 }
-  
-$(document).ready(function(){
-    getYouTubeApiData(displayYoutubeData);
-    fetch(`${apiStore.TMDB.URL}/now_playing`, displayPlayingData);
-    fetch(`${apiStore.TMDB.URL}/140607/recommendations`, displayTMDBSearchData);
+
+$(document).ready(function() {
+  getYouTubeApiData(displayYoutubeData);
+  fetch(`${apiStore.TMDB.URL}/now_playing`, displayPlayingData);
+  fetch(`${apiStore.TMDB.URL}/140607/recommendations`, displayTMDBSearchData);
 });
 
 handleYouTubePlaylistButton();
